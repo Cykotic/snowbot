150aa540dec8cd93966fd62d3b237aa766fd9b48
Aelithria <61665278+Cykotic@users.noreply.github.com>
2025-03-05 13:35:55 -0500

    Updated DB!

--- src/database.js
+++ src/database.js
@@ -1,64 +1,62 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const {
-    mongoURL
-} = process.env;
+const { mongoURL } = process.env;
 
 mongoose.set('strictQuery', true);
 
-mongoose.connect(mongoURL, {
-        keepAlive: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('Successfully connected to mongooseB'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(mongoURL, {
+    keepAlive: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
-// User Schema
+/**
+ * User Schema
+ */
 const userSchema = new mongoose.Schema({
-    userId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    kicks: {
-        type: Number,
-        default: 0,
-    },
+  guildId: { type: String, required: true },
+  userId: { type: String, required: true },
+  kicks: { type: Number, default: 0 }, 
 });
 
-// Click Schema - Logs the users on how many times they bonk someone out
+/**
+ * Click Schema - Tracks users' bonk interactions
+ */
 const clickSchema = new mongoose.Schema({
-    userId: {
-        type: String,
-        required: true,
-    },
-    clicks: {
-        type: Number,
-        default: 0,
-    },
+  guildId: { type: String, required: true },
+  userId: { type: String, required: true },
+  clicks: { type: Number, default: 0 },
 });
 
-// User ID Schema
+/**
+ * User ID Schema
+ */
 const userIdSchema = new mongoose.Schema({
-    userId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    roleIds: {
-        type: [String],
-        default: []
-    }, // Added roles
+  guildId: { type: String, required: true },
+  userId: { type: String, required: true },
+  roleIds: { type: [String], default: [] },
+});
+
+/**
+ * Channel Schema - bonk channels
+ */
+const channelSchema = new mongoose.Schema({
+  guildId: { type: String, required: true, unique: true },
+  channelId: { type: String, required: true },
 });
 
 const ClickStats = mongoose.model('ClickStats', clickSchema);
 const UserStats = mongoose.model('UserStats', userSchema);
 const UserIds = mongoose.model('UserIds', userIdSchema);
+const Channels = mongoose.model('BonkChannels', channelSchema);
 
 module.exports = {
-    UserStats,
-    ClickStats,
-    UserIds,
+  UserStats,
+  ClickStats,
+  UserIds,
+  Channels,
 };
